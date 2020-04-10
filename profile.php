c2a969466e7ac7578d67bab17a6cf370562733fc
Jake Cleland <s3328070@student.rmit.edu.au>
2020-04-10 10:09:47 +1000

    Added index page and added Poppins font to all pages

--- profile.php
+++ profile.php
@@ -22,6 +22,7 @@ global $connect;
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="static/style.css">
     <title>View Profile</title>
